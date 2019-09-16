d8185ffecf0bfb79c3c1f9101d4a5225940b8a73
tpham393 <thuyp@bu.edu>
2019-09-16 11:49:40 -0400

    add sample code for owm api

---
+++ OWM_example.py
@@ -0,0 +1,23 @@
+import pyowm
+
+owm = pyowm.OWM('your-API-key')  # You MUST provide a valid API key
+
+# Have a pro subscription? Then use:
+# owm = pyowm.OWM(API_key='your-API-key', subscription_type='pro')
+
+# Search for current weather in Boston (US) -- must specify ISO 3166 country code
+observation = owm.weather_at_place('Boston, US')
+w = observation.get_weather()
+print(w)                      # <Weather - reference time=2013-12-18 09:20,
+                              # status=Clouds>
+
+# Weather details
+w.get_wind()                  # {'speed': 4.6, 'deg': 330}
+w.get_humidity()              # 87
+w.get_temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+w.get_temperatue('fahrenheit')
+w.get_status()
+
+# Search current weather observations in the surroundings of
+# lat=22.57W, lon=43.12S (Rio de Janeiro, BR)
+observation_list = owm.weather_around_coords(-22.57, -43.12)
