96aca91fb24cb0c78b78d8b3db2770a45e6a0c16
tpham393 <thuyp@bu.edu>
2019-09-28 12:20:05 -0400

    fixes to owm.py

--- Temper/OWM.py
+++ Temper/OWM.py
@@ -25,57 +25,6 @@ def get_weather():
 
     try:
         # Search for current weather in Boston (US) -- must specify ISO 3166 country code
-<<<<<<< HEAD
-        city = input("Please input a US city")
-        
-        observation = owm.weather_at_place(city + ', US')
-
-        if ( observation is not None ):
-            w = observation.get_weather()
-            # Weather details
-            self.wind_speed = w.get_wind()['speed']           # returns dictionary with keys: 'speed', 'deg'
-            self.humidity = w.get_humidity()                  # 87
-            
-            temperature = w.get_temperature('fahrenheit')     # returns a dictionary with keys: 'temp_max', 'temp', and 'temp_min'
-            self.temp_max = temperature['temp_max']
-            self.temp_current = temperature['temp']
-            self.temp_min = temperature['temp_min']
-            
-            self.status = w.get_status()
-            self.detailed_status = w.get_detailed_status()
-        
-        # error handling -- user enters invalid US city
-        else:
-            print("You have entered an invalid city. Returning the weather information for Boston...")
-            observation = owm.weather_at_place('Boston, US')
-            w = observation.get_weather()
-            # Weather details
-            self.wind_speed = w.get_wind()['speed']           # returns dictionary with keys: 'speed', 'deg'
-            self.humidity = w.get_humidity()                  # 87
-            
-            temperature = w.get_temperature('fahrenheit')     # returns a dictionary with keys: 'temp_max', 'temp', and 'temp_min'
-            self.temp_max = temperature['temp_max']
-            self.temp_current = temperature['temp']
-            self.temp_min = temperature['temp_min']
-            
-            self.status = w.get_status()
-            self.detailed_status = w.get_detailed_status()
-
-def get_weather():
-    return weather()
-
-# Test code for above method
-print('----- TESTING OWM MODULE -----')
-w = get_weather()
-print("Wind speed: ", w.wind_speed)
-print("Humidity: ", w.humidity)
-print("High (F): ", w.temp_max)
-print("Current temp (F): ", w.temp_current)
-print("Low (F): ", w.temp_min)
-print("Status (F): ", w.status)
-print("Detailed status (F): ", w.detailed_status)
-print('----- END TEST OWM MODULE -----')
-=======
         city = input("Please enter the name of a US city you would like to get weather info for: ")
 
         try:
@@ -158,4 +107,3 @@ if __name__ == '__main__':
 
     print('\n')
     print('----- END TEST FOR OWM MODULE -----')
->>>>>>> tp-branch-1
